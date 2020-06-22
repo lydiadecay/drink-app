14e2cee6aa3d7235d05fad4f3968208ed245ac27
lydiadecay <57172460+lydiadecay@users.noreply.github.com>
2020-06-22 10:59:46 +0200

    made home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../Styling/home.css";
+import picture from "../Images/cocktails.jpg";
 
 const HomePage = () => (
   <header>
-    <NavLink to="/" activeClassName="is-active">
+    <NavLink
+      to="/"
+      activeClassName="is-active"
+      className="link"
+      style={{ padding: 20 }}
+    >
       Home
     </NavLink>
-    <NavLink to="/explore" activeClassName="is-active">
+    <NavLink to="/explore" activeClassName="is-active" className="link">
       Explore
     </NavLink>
+
     <h1>Welcome to The Cocktail Explorer!</h1>
+
+    <img src={picture} className="pic" alt="cocktails" />
   </header>
 );
 
